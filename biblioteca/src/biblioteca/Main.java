0994bbf53dcf0538546ddc7b21a3b107016976df
BaldonChristian <baldon.christian@gmail.com>
2023-03-01 10:07:15 +0100

    modifica il main, in modo non corretto

--- biblioteca/src/biblioteca/Main.java
+++ biblioteca/src/biblioteca/Main.java
@@ -1,5 +1,7 @@
 package biblioteca;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -13,6 +15,77 @@ public class Main {
 		bb.addLibro("1984","George Orwell","1949","Mondadori",90);
 		
 		if(bb.prendiLibro("Il Gattopardo")) System.out.println("Il titolo corrisponde correttamente.");
+		
+		
+		Scanner scan= new Scanner(System.in);	
+		System.out.println("Benvenuto in biblioteca");
+		System.out.println("Vuoi leggere un libro? (y/n)");
+		String titoloRic="";
+		
+		
+		//aggiungere, cercare, visualizzare e rimuovere 
+		String input= scan.nextLine();
+		if (input.equals("y")) {
+			System.out.println("Che libro vuoi leggere?");
+			titoloRic= scan.nextLine();
+			
+			//metodo per trovare il libro
+			prendiLibro(titoloRic);
+						
+			//metodo per leggere il libro
+		}
+		else if (input.equals("n")){
+			System.out.println("non vuoi leggere");
+			
+			System.out.println("Vuoi aggiungere un libro?");
+			 input= scan.nextLine();
+			if (input.equals("y")) {
+				System.out.println("Inserisci le informazioni");
+				input= scan.nextLine();
+				//metodo per trovare aggiungere un libro
+				}
+			else if(input.equals("n")) {
+				System.out.println("non vuoi aggiungere un libro");
+				
+				System.out.println("Vuoi cancellare un libro?");
+				 input= scan.nextLine();
+				if (input.equals("y")) {
+					System.out.println("Che libro vuoi cancellare?");
+					input= scan.nextLine();
+					
+					//metodo per cancellare un libro
+					RemoveLibro(titoloRic);
+					
+					}
+				else if(input.equals("n")) {
+					System.out.println("non vuoi cancellare un libro");
+				}
+				else {
+					System.out.println("non hai risposto correttamente");
+					//ritorna alla domanda
+				}
+			}
+			else {
+				System.out.println("non hai risposto correttamente");
+				//ritorna alla domanda
+			}
+			
+		} 
+		else {
+			System.out.println("non hai risposto correttamente");
+			//ritorna alla domanda
+		}
+		
+	}
+
+	private static void RemoveLibro(String titoloRic) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void prendiLibro(String ricerca) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
